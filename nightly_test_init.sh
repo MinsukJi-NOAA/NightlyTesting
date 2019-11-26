c9c86955a4c375fcae360dc2ef939533479622c2
MinsukJi-NOAA <Minsuk.Ji@noaa.gov>
2019-11-26 12:46:51 -0500

    Replace apps.txt with repo.info. Output file is now change.csv.

--- nightly_test_init.sh
+++ nightly_test_init.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
-LOGFILE='apps.txt'
 
+REPO_FILE='repo.info'
+LOG_CSV_FILE='change.csv'
+LOG_HTML_FILE='change.html'
+
+# Create LOG_CSV_FILE and enter headers
+if [ ! -f $LOG_CSV_FILE ]; then
+  printf "date," > $LOG_CSV_FILE
+  cat $REPO_FILE | awk -F ' *\\| *' '{printf "%s,", $1}' >> $LOG_CSV_FILE
+  sed -i 's/,$/\n/' $LOG_CSV_FILE
+fi
+
+# Enter today's date (first column in LOG_CSV_FILE)
+printf "$(date -u "+%x-%X")," >> $LOG_CSV_FILE
+
+# Check if repo hash has changed
+# If yes: 1) write 'changed' in LOG_CSV_FILE; 2) update REPO_FILE hash
+# If no: write 'unchanged' in LOG_CSV_FILE
 while IFS="|" read repo url cur_hash
 do
   new_hash=$(git ls-remote ${url} HEAD | awk '{print $1}')
+
   if [ ${cur_hash} != ${new_hash} ]; then
-    echo "*******************************************************"
-    echo "[${repo}] repo has changed since last commit."
-    echo "current hash:${cur_hash}"
-    echo "new hash:    ${new_hash}"
-    # align the above two hash lines for easier comparison
-    echo -e "A new regression test will be conducted.\n"
-    # need to append (update) apps.txt with the new hash
-    # including date may also be helpful
+    #printf "changed (from $(echo ${cur_hash} | cut -c 1-7) to $(echo ${new_hash} | cut -c 1-7))," >> $LOG_CSV_FILE
+    printf "changed ($(echo ${cur_hash} | cut -c 1-7) => $(echo ${new_hash} | cut -c 1-7))," >> $LOG_CSV_FILE
+    #sed
+  else
+    printf "unchanged ($(echo ${cur_hash} | cut -c 1-7))," >> $LOG_CSV_FILE 
+
   fi
-done < ${LOGFILE}
+done < ${REPO_FILE}
+sed -i 's/,$/\n/' $LOG_CSV_FILE
+
+# Convert LOG_CSV_FILE to LOG_HTML_FILE
+
